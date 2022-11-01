minor updates: - fix ddim sampling in training - fix multitag label rescaling in model forward - add gradient accumulation - add sample_bsz

v_diffusion/datasets.py
@@ -42,16 +42,16 @@ class CelebA(datasets.VisionDataset):
             torch.nonzero(data.partition == partition).squeeze()
         self.attr_map = data.attr_names
         if mask == slice(None):  # if split == "all"
-            self.filenames = data.filename
-            self.attrs = data.attr
+            self.filename = data.filename
+            self.attr = data.attr
         else:
-            self.filenames = [data.filename[i] for i in mask]
-            self.attrs = [data.attr[i] for i in mask]
+            self.filename = [data.filename[i] for i in mask]
+            self.attr = [data.attr[i] for i in mask]
         self.download = download
 
     @property
     def targets(self):
-        return self.attrs
+        return self.attr
 
     def _load_data(self):
         CSV = namedtuple("CSV", ["filename", "partition", "attr", "attr_names"])

v_diffusion/diffusion.py
@@ -342,7 +342,7 @@ class GaussianDiffusion:
             t.fill_(ti)
             x_t = self.p_sample_step(
                 denoise_fn, x_t, step=t, y=label, use_ddim=use_ddim)
-        return x_t
+        return x_t.cpu()
 
     @torch.inference_mode()
     def p_sample_progressive(

v_diffusion/models/unet.py
@@ -129,7 +129,8 @@ class UNet(nn.Module):
             apply_attn,
             embedding_dim=None,
             drop_rate=0.,
-            num_heads=1,
+            head_dim=None,
+            num_heads=None,
             num_classes=0,
             multitags=False,
             resample_with_res=True
@@ -146,6 +147,9 @@ class UNet(nn.Module):
         self.apply_attn = apply_attn
         self.num_res_blocks = num_res_blocks
         self.drop_rate = drop_rate
+        if head_dim is None and num_heads is None:
+            num_heads = 1
+        self.head_dim = head_dim
         self.num_heads = num_heads
         self.num_classes = num_classes
         self.multitags = multitags
@@ -174,7 +178,7 @@ class UNet(nn.Module):
         mid_kwargs = dict(embed_dim=self.embedding_dim, drop_rate=drop_rate)
         self.middle = Sequential(
             ResidualBlock(mid_channels, mid_channels, **mid_kwargs),
-            AttentionBlock(mid_channels, num_heads=num_heads),
+            AttentionBlock(mid_channels, head_dim=head_dim, num_heads=num_heads),
             ResidualBlock(mid_channels, mid_channels, **mid_kwargs)
         )
         self.upsamples = nn.ModuleDict(
@@ -194,7 +198,7 @@ class UNet(nn.Module):
             if apply_attn:
                 return Sequential(ResidualBlock(
                     in_chans, out_chans, resampling=resampling, **block_cfgs),
-                    AttentionBlock(out_chans, None, self.num_heads))
+                    AttentionBlock(out_chans, self.head_dim, self.num_heads))
             else:
                 return ResidualBlock(
                     in_chans, out_chans, resampling=resampling, **block_cfgs)
@@ -244,7 +248,7 @@ class UNet(nn.Module):
         if self.num_classes and y is not None:
             if self.multitags:
                 assert y.ndim == 2
-                y.div(torch.count_nonzero(
+                y.div_(torch.count_nonzero(
                     y, dim=1).clamp(min=1.).sqrt().unsqueeze(1))
             t_emb += self.class_embed(y)
         # downsample

v_diffusion/train_utils.py
@@ -70,6 +70,7 @@ class Trainer:
             use_cfg=False,
             use_ema=False,
             grad_norm=1.0,
+            num_accum=1,
             shape=None,
             device=torch.device("cuda" if torch.cuda.is_available() else "cpu"),
             chkpt_intv=5,  # save a checkpoint every [chkpt_intv] epochs
@@ -93,6 +94,7 @@ class Trainer:
         self.scheduler = DummyScheduler() if scheduler is None else scheduler
 
         self.grad_norm = grad_norm
+        self.num_accum = num_accum
         self.device = device
         self.chkpt_intv = chkpt_intv
         self.image_intv = image_intv
@@ -125,28 +127,41 @@ class Trainer:
         assert loss.shape == (B, )
         return loss
 
-    def step(self, x, y):
+    def step(self, x, y, update=True):
         B = x.shape[0]
         loss = self.loss(x, y).mean()
-        loss.backward()
-        # gradient clipping by global norm
-        nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=self.grad_norm)
-        self.optimizer.step()
-        self.optimizer.zero_grad(set_to_none=True)
-        # adjust learning rate every step (warming up)
-        self.scheduler.step()
-        if self.is_main and self.use_ema:
-            self.ema.update()
+        loss.div(self.num_accum).backward()
+        if update:
+            # gradient clipping by global norm
+            nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=self.grad_norm)
+            self.optimizer.step()
+            self.optimizer.zero_grad(set_to_none=True)
+            # adjust learning rate every step (warming up)
+            self.scheduler.step()
+            if self.is_main and self.use_ema:
+                self.ema.update()
         self.stats.update(B, loss=loss.item() * B)
 
-    def sample_fn(self, noises, labels, diffusion=None, use_ddim=False):
+    def sample_fn(
+            self, noises, labels,
+            diffusion=None, use_ddim=False, batch_size=-1):
         if diffusion is None:
             diffusion = self.diffusion
         shape = noises.shape
         with self.ema:
-            sample = diffusion.p_sample(
-                denoise_fn=self.model, shape=shape, device=self.device,
-                noise=noises, label=labels, use_ddim=use_ddim)
+            if batch_size == -1:
+                sample = diffusion.p_sample(
+                    denoise_fn=self.model, shape=shape, device=self.device,
+                    noise=noises, label=labels, use_ddim=use_ddim)
+            else:
+                sample = []
+                for i in range(0, noises.shape[0], batch_size):
+                    _slice = slice(i, i+batch_size)
+                    _shape = (min(noises.shape[0] - i, batch_size), ) + tuple(shape[1:])
+                    sample.append(diffusion.p_sample(
+                        denoise_fn=self.model, shape=_shape, device=self.device,
+                        noise=noises[_slice], label=labels[_slice], use_ddim=use_ddim))
+                sample = torch.cat(sample, dim=0)
         assert sample.grad is None
         return sample
 
@@ -187,7 +202,8 @@ class Trainer:
             labels=None,
             chkpt_path=None,
             image_dir=None,
-            use_ddim=False
+            use_ddim=False,
+            sample_bsz=-1
     ):
 
         if self.is_main and self.num_save_images:
@@ -197,19 +213,26 @@ class Trainer:
             if labels is None and self.num_classes:
                 labels = self.random_labels()
 
+        total_batches = 0
         for e in range(self.start_epoch, self.epochs):
             self.stats.reset()
             self.model.train()
             if isinstance(self.sampler, DistributedSampler):
                 self.sampler.set_epoch(e)
-            with tqdm(self.trainloader, desc=f"{e+1}/{self.epochs} epochs", disable=not self.is_main) as t:
+            with tqdm(
+                    self.trainloader,
+                    desc=f"{e+1}/{self.epochs} epochs",
+                    disable=not self.is_main
+            ) as t:
                 for i, (x, y) in enumerate(t):
+                    total_batches += 1
                     if not self.use_cfg:
                         y = None
                     self.step(
                         x.to(self.device),
                         y.float().to(self.device)
-                        if y is not None else y
+                        if y is not None else y,
+                        update=total_batches % self.num_accum == 0
                     )
                     t.set_postfix(self.current_stats)
                     if i == len(self.trainloader) - 1:
@@ -224,7 +247,8 @@ class Trainer:
                         t.set_postfix(results)
             if self.is_main:
                 if not (e + 1) % self.image_intv and self.num_save_images and image_dir:
-                    x = self.sample_fn(noises, labels, use_ddim=use_ddim).cpu()
+                    x = self.sample_fn(
+                        noises, labels, use_ddim=use_ddim, batch_size=sample_bsz)
                     save_image(x, os.path.join(image_dir, f"{e+1}.jpg"))
                 if not (e + 1) % self.chkpt_intv and chkpt_path:
                     self.save_checkpoint(chkpt_path, epoch=e+1, **results)